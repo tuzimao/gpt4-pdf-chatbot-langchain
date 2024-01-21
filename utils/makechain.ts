ecc18560a354adb8970be9a0a9ab6ced27f09b1e
tuzimao <zliu1@ualberta.ca>
2024-01-20 16:08:18 -0800

    test file update

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -41,7 +41,7 @@ export const makeChain = (retriever: VectorStoreRetriever) => {
 
   const model = new ChatOpenAI({
     temperature: 0, // increase temperature to get more creative answers
-    modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
+    modelName: 'gpt-4-1106-preview', //change this to gpt-4 if you have access
   });
 
   // Rephrase the initial question into a dereferenced standalone question based on
